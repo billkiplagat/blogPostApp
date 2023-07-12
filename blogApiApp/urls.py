da103641d197dfd6525ced68d0648b36320f149b
billkiplagat <bill.kiplagat.7@gmail.com>
2023-07-12 17:33:19 +0300

    Updated create post view

--- blogApiApp/urls.py
+++ blogApiApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('get-all-posts/', views.GetAllPosts)
+    path('get-all-posts/', views.GetAllPosts),
+    path('create-new-post/', views.CreatePost),
+
 ]
--- blogApiApp/views.py
+++ blogApiApp/views.py
@@ -17,3 +17,14 @@ def GetAllPosts(request):
     get_posts = Post.objects.all()
     serializer = PostSerializer(get_posts, many=True)  # many=True mean we are passing a list of objects not 1 item
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def CreatePost(request):
+    data = request.data
+    serializer = PostSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"Success": "The post was created"}, status=201)
+    else:
+        return Response(serializer.errors, status=400)
